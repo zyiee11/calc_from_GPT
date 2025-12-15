d7979cf6cb9efa66ac82b6b680567827831c9abe
zyiee11 <bff345.ziber@gmail.com>
2025-12-15 21:36:42 +0300

    Add files via upload

---
+++ House Robber.py
@@ -0,0 +1,19 @@
+def rob(nums):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+    
+    prev2 = nums[0]
+    prev1 = max(nums[0], nums[1])
+    
+    for i in range(2, len(nums)):
+        current = max(prev1, prev2 + nums[i])
+        prev2 = prev1
+        prev1 = current
+    
+    return prev1
+
+if __name__ == "__main__":
+    print(rob([1, 2, 3, 1]))
+    print(rob([2, 7, 9, 3, 1]))
